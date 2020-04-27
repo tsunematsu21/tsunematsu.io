dc3866fa4e246f6a141a76829ecae17d71282860
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2020-04-27 23:51:34 +0900

    Change layout in nagesen page

--- pages/nagesen.vue
+++ pages/nagesen.vue
@@ -5,120 +5,122 @@
       In Japan, sending money and gifts for support on the Internet is known as
       <em>Nagesen</em>.
     </p>
-    <div class="has-text-left">
-      <div class="card has-background-main">
-        <div class="card-content">
-          <section class="content has-text-centered">
-            <h2><i class="fas fa-coins" /> Would you support me?</h2>
-            <p>
-              Please support me if you like. Gladly accepting support in several
-              ways.
-            </p>
-            <hr />
-            <div class="columns is-desktop">
-              <div class="column">
-                <section class="has-text-centered">
-                  <h3 class="has-text-primary">
+    <div class="columns">
+      <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <section class="content">
+              <h2><i class="fas fa-heart" /> Would you support me?</h2>
+              <p>
+                Please support me if you like. Gladly accepting support in
+                several ways.
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-flex">
+        <div class="card is-flex">
+          <div class="card-content is-flex">
+            <section class="content">
+              <h3>Support on <span class="has-text-primary">Kyash</span></h3>
+              <div class="columns">
+                <div class="column">
+                  <a href="kyash://qr/u/7919347958298828142"
+                    ><img src="~/assets/kyash_qrcode.png"
+                  /></a>
+                </div>
+                <div class="column">
+                  <p class="has-text-left">
+                    With
                     <a
                       href="https://kyash.co/"
                       target="_blank"
                       class="has-text-primary"
                       >Kyash</a
                     >
-                  </h3>
-                  <div class="columns">
-                    <div class="column">
-                      <a href="kyash://qr/u/7919347958298828142"
-                        ><img src="~/assets/kyash_qrcode.png"
-                      /></a>
-                    </div>
-                    <div class="column">
-                      <p class="has-text-left">
-                        With Kyash accounts, you can send real-time money
-                        anytime, anywhere with no fees.<br />
-                        (See
-                        <a
-                          href="https://kyash.co/"
-                          target="_blank"
-                          class="has-text-primary"
-                          >https://kyash.co/</a
-                        >)<br />
-                        Your support will help my activity at whatever price.
-                      </p>
-                    </div>
-                  </div>
-                </section>
-              </div>
-              <div class="column is-desktop">
-                <section class="has-text-centered">
-                  <h3 class="has-text-primary">
-                    Ko-fi
-                  </h3>
-                  <p class="has-text-left">
-                    On my Ko-fi page, accepting support by price of a coffee. It
-                    is friendly and free way for support my work. Please visit
-                    my Ko-fi page from the button below.<br />
+                    accounts, you can send real-time money anytime, anywhere
+                    with no fees.<br />
+                    Your support will help my activity at whatever price.
                   </p>
-                  <div class="has-text-centered">
-                    <a
-                      href="https://ko-fi.com/tsunematsu21"
-                      target="_blank"
-                      class="button is-ko-fi"
-                    >
-                      <span class="icon">
-                        <i class="fas fa-coffee" />
-                      </span>
-                      <span>Support Me on Ko-fi</span>
-                    </a>
-                  </div>
-                </section>
+                </div>
               </div>
-              <div class="column is-desktop">
-                <section class="has-text-centered">
-                  <h3 class="has-text-primary">
-                    Feedback
-                  </h3>
-                  <p class="has-text-left">
-                    Please give me feedback on
-                    <nuxt-link to="/portfolio" class="has-text-primary"
-                      >my works</nuxt-link
-                    >. It's encourage me more than you think. Accepting your
-                    feedback in
-                    <nuxt-link to="/contact" class="has-text-primary">
-                      contact
-                    </nuxt-link>
-                    page and social media.
-                  </p>
-                  <SocialButtons
-                    buttons-classes="justify-content-center"
-                    button-classes="is-black is-outlined is-small"
-                    is-icon-only="true"
-                  />
-                </section>
-              </div>
-            </div>
-            <div class="columns">
-              <div class="column is-desktop">
-                <section class="has-text-centered">
-                  <h3 class="has-text-primary">Other option</h3>
-                  <p>Getting Ready.</p>
-                </section>
-              </div>
-            </div>
-          </section>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div class="column is-flex">
+        <div class="card is-flex">
+          <div class="card-content is-flex">
+            <section class="content">
+              <h3>Support on <span class="has-text-primary">Ko-fi</span></h3>
+              <p class="has-text-left">
+                On my Ko-fi page, accepting support by price of a coffee. It is
+                friendly and free way for support my work. Please visit my Ko-fi
+                page from the button below.<br />
+              </p>
+            </section>
+            <a
+              href="https://ko-fi.com/tsunematsu21"
+              target="_blank"
+              class="button is-ko-fi"
+            >
+              <span class="icon">
+                <i class="fas fa-coffee" />
+              </span>
+              <span>Support Me on Ko-fi</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="column is-flex">
+        <div class="card is-flex">
+          <div class="card-content is-flex">
+            <section class="content">
+              <h3 class="has-text-primary">
+                Feedback
+              </h3>
+              <p class="has-text-left">
+                Please give me feedback on
+                <nuxt-link to="/portfolio" class="has-text-primary"
+                  >my works</nuxt-link
+                >. It's encourage me more than you think. Accepting your
+                feedback in contact page.
+              </p>
+            </section>
+            <router-link to="portfolio" class="button is-primary">
+              <span class="icon">
+                <i class="fas fa-envelope" />
+              </span>
+              <span>Send Feedback</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-one-third">
+        <div class="card">
+          <div class="card-content">
+            <section class="content">
+              <h3 class="has-text-primary">Other option</h3>
+              <p>Getting Ready.</p>
+            </section>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import SocialButtons from '~/components/SocialButtons.vue'
-
-export default Vue.extend({
-  components: {
-    SocialButtons
+<style lang="scss" scoped>
+div.card-content.is-flex {
+  flex-direction: column;
+  > .button {
+    margin-top: auto;
   }
-})
-</script>
+}
+</style>
